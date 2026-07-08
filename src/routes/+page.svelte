80cdab9057b35da56ffedd48abe019588f412713
JessicaDinova <dino0001@hz.nl>
2026-07-08 15:56:51 +0200

    added more boilerplates and made the styling of home page applied to every other child

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,21 @@
 <script>
+  import cv from "$lib/assets/cv/cv.pdf"
   import star from "$lib/assets/stars/starOne.png";
   import Home from "$lib/components/Home.svelte";
 </script>
 
-<nav
-  class="w-full items-center fixed z-10 text-3xl justify-between flex px-16 py-7 font-light"
->
+<nav class="w-full items-center fixed z-10 text-2xl justify-between flex px-16 py-6 font-light bg-cream-100">
   <a href="#home">Home</a>
   <a href="#about">About Me</a>
   <a href="#skills">Skills</a>
   <a href="#projects">Projects</a>
-  <a class="flex flex-row items-center" href="/">
+  <a class="flex flex-row items-center" target="_blank" href="{cv}">
     <img class="h-8" src={star} alt="star"/>CV
-    <img class="h-8" src={star} alt="star"/></a
-  >
+    <img class="h-8" src={star} alt="star"/>
+  </a>
 </nav>
 
-<div class="flex flex-col">
+<div class=" *:even:bg-coal-100 *:snap-start flex flex-col w-screen h-screen snap-y snap-proximity overflow-y-scroll">
   <div id="home">
     <Home />
   </div>
